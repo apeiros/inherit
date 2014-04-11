75b1457def4cb4ab5780befb38bfbe17786070d2
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2014-04-11 13:46:58 +0200

    Use refinements instead of monkey patching.
    Hide the modules in instance variabels instead of constants.
    
    Enable ancestry checking to work properly by include-/extending the inherited module directly.

--- lib/inherit.rb
+++ lib/inherit.rb
@@ -12,87 +12,118 @@ require 'inherit/version'
 # @example Usage
 #     require 'inherit'
 #
-#     module Inheritable
-#       module Constants
-#         Foo = "Nice constant!"
-#       end
-#       module ClassMethods
-#         def funky
-#           "Funky class method!"
-#         end
-#       end
-#       module InstanceMethods
-#         def rad
-#           "Rad instance method!"
-#         end
-#       end
-#       def self.inherited(subclass)
-#         puts "#{self} got inherited by #{subclass}"
-#       end
-#       def self.extended(object)
-#         puts "#{self} extended #{object}"
-#       end
-#     end
+#     module MyProject
+#       using Inherit
 #     
-#     class Example
-#       inherit Inheritable # -> "Inheritable got inherited by Example"
+#       module Inheritable
+#         inheritable_constants do
+#           Foo = "Nice constant!"
+#         end
+#         inheritable_class_methods do
+#           def funky
+#             "Funky class method!"
+#           end
+#         end
+#         inheritable_instance_methods do
+#           def rad
+#             "Rad instance method!"
+#           end
+#         end
+#         def self.inherited(subclass)
+#           puts "#{self} got inherited by #{subclass}"
+#         end
+#         def self.extended(object)
+#           puts "#{self} extended #{object}"
+#         end
+#       end
+#     
+#       class Example
+#         inherit Inheritable # -> "Inheritable got inherited by Example"
+#       end
+#       p(Example::Foo)                  # => "Nice constant!"
+#       p(Example.funky)                 # => "Funky class method!"
+#       p(Example.new.rad)               # => "Rad instance method!"
+#       a_string = "a string"
+#       a_string.inherit Inheritable     # -> "Inheritable extended a string"
+#       p(a_string.singleton_class::Foo) # => "Nice constant!"
+#       p(a_string.rad)                  # => "Rad instance method!"
 #     end
-#     Example::Foo                  # => "Nice constant!"
-#     Example.funky                 # => "Funky class method!"
-#     Example.new.rad               # => "Rad instance method!"
-#     a_string = "a string"
-#     a_string.inherit Inheritable  # -> "Inheritable extended a string"
-#     a_string.singleton_class::Foo # => "Nice constant!"
-#     a_string.rad                  # => "Rad instance method!"
 module Inherit
-end
 
-class Module
+  refine Module do
 
-  # Add constants, class methods and instance methods to a module/class.
-  #
-  # Adds the following things (if present) from `ancestor` to the module/class:
-  #
-  # * includes ancestor::Constants
-  # * extends ancestor::ClassMethods
-  # * includes ancestor::InstanceMethods
-  #
-  # Additionally it triggers `ancestor.inherited` with the module/class as argument.
-  #
-  # @param [Module] ancestor
-  #   The module to add constants, class methods and instance methods from, and
-  #   to trigger `.inherited` on.
-  #
-  # @see Inherit Usage example
-  #
-  def inherit(ancestor)
-    include ancestor::Constants if ancestor.const_defined?(:Constants)
-    extend  ancestor::ClassMethods if ancestor.const_defined?(:ClassMethods)
-    include ancestor::InstanceMethods if ancestor.const_defined?(:InstanceMethods)
-    ancestor.inherited(self) if ancestor.respond_to?(:inherited)
+    # Add constants, class methods and instance methods to a module/class.
+    #
+    # Adds the following things (if present) from `ancestor` to the module/class:
+    #
+    # * includes ancestor::Constants
+    # * extends ancestor::ClassMethods
+    # * includes ancestor::InstanceMethods
+    #
+    # Additionally it triggers `ancestor.inherited` with the module/class as argument.
+    #
+    # @param [Module] ancestor
+    #   The module to add constants, class methods and instance methods from, and
+    #   to trigger `.inherited` on.
+    #
+    # @see Inherit Usage example
+    #
+    def inherit(ancestor)
+      include ancestor.inheritable_constants if ancestor.inheritable_constants?
+      extend  ancestor.inheritable_class_methods if ancestor.inheritable_class_methods?
+      include ancestor.inheritable_instance_methods if ancestor.inheritable_instance_methods?
+      ancestor.inherited(self) if ancestor.respond_to?(:inherited)
+      include ancestor
+    end
+
+    def inheritable_constants(&block)
+      @inheritable_constants = Module.new(&block) if block
+      @inheritable_constants
+    end
+
+    def inheritable_class_methods(&block)
+      @inheritable_class_methods = Module.new(&block) if block
+      @inheritable_class_methods
+    end
+
+    def inheritable_instance_methods(&block)
+      @inheritable_instance_methods = Module.new(&block) if block
+      @inheritable_instance_methods
+    end
+
+    def inheritable_constants?
+      @inheritable_constants ? true : false
+    end
+
+    def inheritable_class_methods?
+      @inheritable_constants ? true : false
+    end
+
+    def inheritable_instance_methods?
+      @inheritable_constants ? true : false
+    end
   end
-end
 
-class Object
-
-  # Add constants and instance methods to an object.
-  #
-  # Adds the following things (if present) from `ancestor` to the object:
-  #
-  # * extends ancestor::Constants
-  # * extends ancestor::InstanceMethods
-  #
-  # Additionally it triggers `ancestor.extended` with the object as argument.
-  #
-  # @param [Module] ancestor
-  #   The module to add constants and instance methods from, and to trigger
-  #   `.extended` on.
-  #
-  # @see Inherit Usage example
-  #
-  def inherit(ancestor)
-    extend ancestor::Constants if ancestor.const_defined?(:Constants)
-    extend ancestor::InstanceMethods if ancestor.const_defined?(:InstanceMethods)
-    ancestor.extended(self) if ancestor.respond_to?(:extended)
+  refine Object do
+    # Add constants and instance methods to an object.
+    #
+    # Adds the following things (if present) from `ancestor` to the object:
+    #
+    # * extends ancestor::Constants
+    # * extends ancestor::InstanceMethods
+    #
+    # Additionally it triggers `ancestor.extended` with the object as argument.
+    #
+    # @param [Module] ancestor
+    #   The module to add constants and instance methods from, and to trigger
+    #   `.extended` on.
+    #
+    # @see Inherit Usage example
+    #
+    def inherit(ancestor)
+      extend ancestor.inheritable_constants if ancestor.inheritable_constants
+      extend ancestor.inheritable_instance_methods if ancestor.inheritable_instance_methods
+      extend ancestor
+    end
   end
 end
